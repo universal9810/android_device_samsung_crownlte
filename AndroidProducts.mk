2cca1d60867034ac367ef41b76976ed398b2b444
baddar90 <mnbaddar1990@gmail.com>
2022-03-08 23:05:59 +0400

    crownlte: arrow bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_crownlte.mk
+    $(LOCAL_DIR)/arrow_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_crownlte-eng \
-    lineage_crownlte-user \
-    lineage_crownlte-userdebug
+    arrow_crownlte-eng \
+    arrow_crownlte-user \
+    arrow_crownlte-userdebug
--- arrow_crownlte.mk
+++ arrow_crownlte.mk
@@ -28,13 +28,16 @@ TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_crownlte
+PRODUCT_NAME := arrow_crownlte
 PRODUCT_DEVICE := crownlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N960F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# ArrowOS Properties
+DEVICE_MAINTAINER := Baddar90
