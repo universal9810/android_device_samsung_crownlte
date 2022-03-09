a3f40c96dc9b058cad6582718ef84b068f50c387
Cyder <87647653+CyderVII28x03@users.noreply.github.com>
2022-03-09 17:55:56 +0700

    DerpFest-12

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_crownlte.mk
+    $(LOCAL_DIR)/derp_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_crownlte-eng \
-    arrow_crownlte-user \
-    arrow_crownlte-userdebug
+    derp_crownlte-eng \
+    derp_crownlte-user \
+    derp_crownlte-userdebug
