35f3b38b4317ae58f44121dc349407a2bb50b793
Dylan Neve <dylanneve1@gmail.com>
2022-03-09 01:09:39 +0000

    Bring-up for Pixel Experience

--- aosp_crownlte.mk
+++ aosp_crownlte.mk
@@ -27,27 +27,14 @@ $(call inherit-product, device/samsung/crownlte/device.mk)
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+## Inherit some common AOSP stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := arrow_crownlte
+PRODUCT_NAME := aosp_crownlte
 PRODUCT_DEVICE := crownlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N960F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-# ArrowOS Properties
-DEVICE_MAINTAINER := Baddar90
-
-BUILD_FINGERPRINT := "samsung/crownltexx/crownlte:10/QP1A.190711.020/N960FXXU9FUK1:user/release-keys"
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=crownltexx \
-    PRIVATE_BUILD_DESC="crownltexx-user 10 QP1A.190711.020 N960FXXU9FUK1 release-keys"
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.PDA=N960FXXU9FUK1 \
-    ro.build.fingerprint=samsung/crownltexx/crownlte:10/QP1A.190711.020/N960FXXU9FUK1:user/release-keys
