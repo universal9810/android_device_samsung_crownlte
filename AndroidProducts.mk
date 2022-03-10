c39a96d8e90f7d83100181964bd2175506ad6bb7
Cyder <87647653+CyderVII28x03@users.noreply.github.com>
2022-03-10 13:13:06 +0700

    OctaviOS-Twelve bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_crownlte.mk
+    $(LOCAL_DIR)/octavi_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_crownlte-eng \
-    arrow_crownlte-user \
-    arrow_crownlte-userdebug
+    octavi_crownlte-eng \
+    octavi_crownlte-user \
+    octavi_crownlte-userdebug
--- octavi_crownlte.mk
+++ octavi_crownlte.mk
@@ -28,10 +28,14 @@ TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+
+# Custom stuff
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGER := true
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := arrow_crownlte
+PRODUCT_NAME := octavi_crownlte
 PRODUCT_DEVICE := crownlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N960F
