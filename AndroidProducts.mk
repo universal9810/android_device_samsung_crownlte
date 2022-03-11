2f0fa488417740c8fea648b296f62b8bd4a9b2fd
Cyder <87647653+CyderVII28x03@users.noreply.github.com>
2022-03-11 21:02:51 +0700

    Evolution-Snow bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_crownlte.mk
+    $(LOCAL_DIR)/evolution_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_crownlte-eng \
-    arrow_crownlte-user \
-    arrow_crownlte-userdebug
+    evolution_crownlte-eng \
+    evolution_crownlte-user \
+    evolution_crownlte-userdebug
